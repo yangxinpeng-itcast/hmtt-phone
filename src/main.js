d7b4f849b449e05cb558d37d8bc42716206ea122
yangxinpeng <972672845@qq.com>
2019-07-29 20:52:41 +0800

    表单验证

--- src/main.js
+++ src/main.js
@@ -5,8 +5,25 @@ import vant from 'vant'
 import 'vant/lib/index.css'
 import 'amfe-flexible/index.min.js'
 import store from './store/index.js'
+import VeeValidate, {
+  Validator
+} from 'vee-validate'
+
+import zh_CN from 'vee-validate/dist/locale/zh_CN'
 
 Vue.use(vant)
+Vue.use(VeeValidate)
+Validator.localize('zh_CN', zh_CN)
+
+// 自定义验证规则
+
+Validator.extend('phone', {
+  getMessage: field => field + '不正确',
+  validate: value => {
+    // 自己写手机号的验证规则 -> 验证形参value是否符合手机号
+    return value.length === 11 && /^((13|15|18|17)[0-9]{1})\d{8}$/.test(value)
+  }
+})
 
 Vue.config.productionTip = false
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="box">
     <van-nav-bar title="登录" />
-    <van-cell-group>
-      <van-field v-model="user.mobile" required label="手机号" placeholder="手机号" />
+    <form action="/">
+      <van-cell-group>
+        <van-field
+          v-validate="'required|phone'"
+          name="phone"
+          v-model="user.mobile"
+          :error-message="errors.first('phone')"
+          label="手机号"
+          placeholder="请输入用户名"
+          required
+        />
 
-      <van-field v-model="user.code" type="password" label="验证码" placeholder="验证码" required />
-    </van-cell-group>
-    <van-button type="info" @click.prevent="userLogin">登录</van-button>
+        <van-field v-model="user.code" label="验证码" placeholder="请输入验证码" required type="password" />
+      </van-cell-group>
+      <!-- @事件名.修饰符 -->
+      <van-button type="info" @click.prevent="userLogin">登录</van-button>
+    </form>
+    
   </div>
 </template>
 
 <script>
 // import request from '../../utils/request.js'
-import { login } from '@/api/user.js'
+import { login } from "@/api/user.js";
 export default {
   data() {
     return {
@@ -24,10 +36,10 @@ export default {
   },
   methods: {
     async userLogin() {
-        const res = await login(this.user);
-        this.$store.commit('setUser',res)
-        console.log(res)
-      } 
+      const res = await login(this.user);
+      this.$store.commit("setUser", res);
+      console.log(res);
+    }
   }
 };
 </script>
