03f7a210eb213121f6a464de28348f463e5ba97a
yangxinpeng <972672845@qq.com>
2019-07-29 14:50:06 +0800

    登录页布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,35 @@
 <template>
-    <div class="box">login</div>
+  <div class="box">
+    <van-nav-bar title="登录" left-arrow />
+    <van-cell-group>
+      <van-field v-model="user.mobile" required label="手机号" placeholder="手机号" />
+
+      <van-field v-model="user.code" type="password" label="验证码" placeholder="验证码" required />
+    </van-cell-group>
+    <van-button type="info">登录</van-button>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped lang="less">
+.box {
+  .van-nav-bar {
+    background-color: #0096fa;
+  }
+  .van-button {
+    width: 100%;
+  }
+}
 </style>
